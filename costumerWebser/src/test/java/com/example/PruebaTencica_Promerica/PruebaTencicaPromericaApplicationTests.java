b6b2c3c378df41c542de3eda818fb265c5b2f0d4
mainorblanco9 <64112058+mainorblanco9@users.noreply.github.com>
2024-01-14 00:53:45 -0600

    Pruebas Unitarias listas
    Termino proyecto

--- costumerWebser/src/test/java/com/example/PruebaTencica_Promerica/PruebaTencicaPromericaApplicationTests.java
+++ costumerWebser/src/test/java/com/example/PruebaTencica_Promerica/PruebaTencicaPromericaApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.PruebaTencica_Promerica;
 
+import com.example.PruebaTencica_Promerica.dto.customerDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -17,32 +26,39 @@ class PruebaTencicaPromericaApplicationTests {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	@Test
 	void testCreateCustomer() throws Exception {
+		customerDto customer = new customerDto("451147414", "PruebaUnitaria", "ApellidoPrueba", "021101744", "1990-01-01");
+		String customerJson = objectMapper.writeValueAsString(customer);
 
-		String customerJson = "{ \"id_cedula\": \"504110673\", \"firstName\": \"Minor\", \"lastName\": \"Blanco\", \"phone\": \"83010\", \"birthdate\": \"1990-01-01\" }";
-
-		// Realiza la solicitud POST y espera un status 201 (CREATED)
-		mockMvc.perform(post("/create")
+		ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/customer/create")
 						.contentType(MediaType.APPLICATION_JSON)
 						.content(customerJson))
 				.andExpect(status().isCreated());
 	}
-
 	@Test
 	void testDeleteCustomer() throws Exception {
-		mockMvc.perform(delete("/customer/delete/{id}", "123456"))
+		mockMvc.perform(delete("/customer/delete/{id_cedula}", "504110673"))
 				.andExpect(status().isOk());
 	}
 
 	@Test
 	void testUpdateCustomer() throws Exception {
+		customerDto customer = new customerDto("451147414", "UpdatedFirstName", "UpdatedLastName", "UpdatedPhone", "1990-01-01");
+		String customerJson = objectMapper.writeValueAsString(customer);
 
+		mockMvc.perform(MockMvcRequestBuilders
+						.request(HttpMethod.PUT, "/customer/update")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(customerJson.getBytes()))
+				.andExpect(status().isOk());
 	}
-
 	@Test
 	void testGetCustomerById() throws Exception {
-		mockMvc.perform(get("/customer/getById/{id_cedula}", "123456"))
+		mockMvc.perform(get("/customer/getById/{idCedula}", "504110673"))
 				.andExpect(status().isOk());
 	}
 
